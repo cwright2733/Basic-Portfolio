2b639b967c28603db69fefc098f50404751cc1df
cameron wright <arabia.cw@gmail.com>
2019-03-30 21:54:03 -0400

    update

---
+++ assets/javascript/Portfolio.js
@@ -0,0 +1,15 @@
+//open nav bar function
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250";
+}
+
+function closeNav(){
+    document.getElementById("mySideav").style.width = "0";
+}
+
+
+
+
+
+
+//close navbar
